11e98d9c801c774c2555eb53aac182f4375bdc74
Владимир <uipertest@gmail.com>
2025-03-29 18:59:07 +0700

    Fix build configuration

--- carstyle/vite.config.js
+++ carstyle/vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['react-animated-cursor']
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3002',
-        changeOrigin: true
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
       }
     }
+  },
+  server: {
+    port: 3000
   }
 })
