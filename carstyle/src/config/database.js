757757ebecb6ea878914136f06919738440db1a7
Владимир <uipertest@gmail.com>
2025-03-29 19:17:34 +0700

    Update database configuration for PlanetScale

--- carstyle/src/config/database.js
+++ carstyle/src/config/database.js
@@ -10,10 +10,13 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, '../..', '.env') });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
+  host: process.env.DB_HOST || 'aws.connect.psdb.cloud',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'carstyle',
+  ssl: {
+    rejectUnauthorized: true
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
